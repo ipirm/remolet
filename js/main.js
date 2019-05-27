f1462423d23f0ae4ad1d272c2059568ee96b1b71
Ilham <48025760+ipirm@users.noreply.github.com>
2019-05-27 09:34:20 +0400

    New site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+   var swiper = new Swiper('#js-inner_slider', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	    pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+ $(".variables-nav__item").click(function() {
+    $(this).toggleClass("active");
+    $(this).next(".table-bordered").slideToggle("slow");
+  });
